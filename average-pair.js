3b8a5190641801a5dce42d6e65c6795fd03e1252
Jay25102 <jasonhuang25102@gmail.com>
2024-08-20 16:01:01 -0400

    average pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(inputArr, targetAvg) {
+    let left = 0;
+    let right = inputArr.length - 1;
+    let pairAvg;
+
+    while (left < right) {
+        pairAvg = (inputArr[left] + inputArr[right]) / 2;
+        if (pairAvg === targetAvg) return true;
+        else if (pairAvg < targetAvg) left++;
+        else if (pairAvg > targetAvg) right--;
+    }
+
+    return false;
 }
+
+module.exports = averagePair;
