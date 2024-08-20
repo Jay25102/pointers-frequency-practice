bd2144a1fb9d395b1fa6b1edd3c6ecf70b3822ec
Jay25102 <jasonhuang25102@gmail.com>
2024-08-20 16:22:37 -0400

    separate positive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    let temp;
+
+    while (left < right) {
+        if ((arr[left] < 0 && arr[right] > 0)) {
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+        else if (arr[left] < 0 && arr[right] < 0) {
+            right--;
+        }
+        else {
+            left++;
+        }
+    }
+
+    return arr;
 }
+
+module.exports = separatePositive;
