181e11f4ae796914d5432c943b85fba29ae51e7f
Jay25102 <jasonhuang25102@gmail.com>
2024-08-20 16:26:52 -0400

    is subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    let idx1 = 0;
+    let idx2 = 0;
+
+    while (idx2 < str2.length) {
+        if (str2[idx2] === str1[idx1]) {
+            idx1++;
+        }
+        if (idx1 === str1.length) return true;
+        idx2++;
+    }
+    
+    return false;
 }
+
+module.exports = isSubsequence;
