a424f1e238af1f8122b7588f08c25276c73e3f69
Jay25102 <jasonhuang25102@gmail.com>
2024-08-20 15:51:19 -0400

    construct note

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary
-function constructNote() {
+function freqCounter(str) {
+    let strFreqs = {};
+
+    for (let val of str) {
+        let valCount = strFreqs[val] || 0;
+        strFreqs[val] = valCount + 1;
+    }
+
+    return strFreqs;
 }
+
+// add whatever parameters you deem necessary
+function constructNote(message, letters) {
+    if (letters.length = 0) return false;
+    if (message.length > letters.length) return false;
+
+    let messageCounter = freqCounter(message);
+    let lettersCounter = freqCounter(letters);
+
+    for (let key in messageCounter) {
+        if (!(lettersCounter[key] >= messageCounter[key])) return false;
+    }
+
+    return true;
+}
+
+module.exports = constructNote;
