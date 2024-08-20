41a9426858c458e712de5ef0a7a7510bdc3d3776
Jay25102 <jasonhuang25102@gmail.com>
2024-08-20 16:16:49 -0400

    same freq

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+function freqCounter(str) {
+    let strFreqs = {};
+
+    for (let val of str) {
+        let valCount = strFreqs[val] || 0;
+        strFreqs[val] = valCount + 1;
+    }
+
+    return strFreqs;
 }
+
+// add whatever parameters you deem necessary
+function sameFrequency(int1, int2) {
+    int1String = int1.toString();
+    int2String = int2.toString();
+    if (int1String.length !== int2String.length) return false;
+
+    let int1Counter = freqCounter(int1String);
+    let int2Counter = freqCounter(int2String);
+
+    for (let key in int1Counter) {
+        if (int1Counter[key] !== int2Counter[key]) return false;
+    }
+    return true;
+}
+
+module.exports = sameFrequency;
