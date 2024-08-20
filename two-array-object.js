1e35b5f5b815ac929117e505d0b78e0e8050ca2b
Jay25102 <jasonhuang25102@gmail.com>
2024-08-20 16:10:11 -0400

    two array

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,24 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+    let result = {};
+    let max;
+    if (arr2 > arr1) {
+        max = arr2.length;
+    }
+    else {
+        max = arr1.length;
+    }
+
+    for (let i = 0; i < max; i++) {
+        if (arr2[i] === undefined) {
+            result[arr1[i]] = null;
+        }
+        else {
+            result[arr1[i]] = arr2[i];
+        }
+    }
+
+    return result;
 }
+
+module.exports = twoArrayObject;
